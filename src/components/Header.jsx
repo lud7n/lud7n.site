8d88cd05d22873aa534aa846070147ab09348917
lud7n <kudou0821@icloud.com>
2024-08-15 18:06:51 +0900

    20240815

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import './Header.css';  // CSSファイルをインポート
+import logoImage from '../Images/lud7n.logo_0815.png'; // ロゴ画像のパスを適切に設定してください
 
 const Header = () => {
     return (
         <header className="header">
             <div className='logo'>
+                <img src={logoImage} alt="lud7n.site Logo" className="logo-image" />
                 <h3><nav><ul><li><Link to="/">lud7n.site</Link></li></ul></nav></h3>
             </div>
 
             <nav>
                 <ul>
                     <li>
-                        <Link to="/">home</Link>
+                        <Link to="/">Home</Link>
                     </li>
                     <li>
-                        <Link to="/blog">blog</Link>
+                        <Link to="/blog">Blog</Link>
                     </li>
                 </ul>
             </nav>
