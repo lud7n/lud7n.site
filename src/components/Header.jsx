7c58f215d4839479d6026e3acfebefb2bd2933f0
lud7n <kudou0821@icloud.com>
2024-08-15 21:22:04 +0900

    20240815

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,9 +14,6 @@ const Header = () => {
             <nav>
                 <ul>
                     <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
                         <Link to="/blog">Blog</Link>
                     </li>
                 </ul>
